3d35bebf9f23030232f865bb3095cbe319466cb7
Yurii Kit <insidelkit@gmail.com>
2025-01-26 19:34:14 +0200

    homework

--- js/task-3.js
+++ js/task-3.js
@@ -1,17 +1,21 @@
-function filterArray(numbers, value) {
-	let filterNumbers = [];
-	for (let i = 0; i < numbers.length; i++) {
-		if (numbers[i] > value) {
-			filterNumbers.push(numbers[i]);
-		}
-	}
-	return filterNumbers;
-}
-/*function filterArray(numbers, value) {
-    return numbers.filter(number => number > value);
-}*/
-console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
-console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
-console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
-console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+const profile = {
+	username: "Jacob",
+	playTime: 300,
+	changeUsername(newName) {
+		profile.username = newName;
+	},
+	updatePlayTime(hours) {
+		profile.playTime += hours;
+	},
+	getInfo() {
+		return `${profile.username} has ${profile.playTime} active hours!`;
+	},
+};
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
